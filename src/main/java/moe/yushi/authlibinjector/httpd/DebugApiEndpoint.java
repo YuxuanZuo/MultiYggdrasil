yushijinhun/authlib-injector

more performance metrics

src/main/java/moe/yushi/authlibinjector/httpd/DebugApiEndpoint.java
@@ -33,8 +33,12 @@ public class DebugApiEndpoint {
 		if (session.getUri().equals("/debug/metrics") && session.getMethod().equals("GET")) {
 			PerformanceMetrics metrics = AuthlibInjector.getClassTransformer().performanceMetrics;
 			JSONObject response = new JSONObject();
-			response.put("classesProcessed", metrics.getClassesProcessed());
 			response.put("totalTime", metrics.getTotalTime());
+			response.put("matchTime", metrics.getMatchTime());
+			response.put("scanTime", metrics.getScanTime());
+			response.put("analysisTime", metrics.getAnalysisTime());
+			response.put("classesScanned", metrics.getClassesScanned());
+			response.put("classesSkipped", metrics.getClassesSkipped());
 			return Response.newFixedLength(Status.OK, CONTENT_TYPE_JSON, response.toJSONString());
 		} else {
 			return Response.newFixedLength(Status.NOT_FOUND, null, null);

YuxuanZuo/MultiYggdrasil

src/main/java/moe/yushi/authlibinjector/httpd/DebugApiEndpoint.java
@@ -34,8 +34,12 @@ public class DebugApiEndpoint {
 		if (session.getUri().equals("/debug/metrics") && session.getMethod().equals("GET")) {
 			PerformanceMetrics metrics = AuthlibInjector.getClassTransformer().performanceMetrics;
 			JsonObject response = new JsonObject();
-			response.addProperty("classesProcessed", metrics.getClassesProcessed());
 			response.addProperty("totalTime", metrics.getTotalTime());
+			response.addProperty("matchTime", metrics.getMatchTime());
+			response.addProperty("scanTime", metrics.getScanTime());
+			response.addProperty("analysisTime", metrics.getAnalysisTime());
+			response.addProperty("classesScanned", metrics.getClassesScanned());
+			response.addProperty("classesSkipped", metrics.getClassesSkipped());
 			return Response.newFixedLength(Status.OK, CONTENT_TYPE_JSON, toJsonString(response));
 		} else {
 			return Response.newFixedLength(Status.NOT_FOUND, null, null);